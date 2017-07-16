81ea20d52fe2eeddf2a38967334ee7df135b1167
brianlam37 <brianlam37@gmail.com>
2017-07-15 21:05:44 -0700

    made a lot of aesthetic changes, possibly bad ones

--- javascript.js
+++ javascript.js
@@ -18,3 +18,49 @@ window.onclick = function(event) {
     }
   }
 }
+
+/*****************
+HOME
+*****************/
+/*****************
+SOURCE CODE FOUND AT https://www.w3schools.com/w3css/w3css_slideshow.asp
+Combined two functions for my preferences and was edited according to my needs
+*****************/
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("buttonNum");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace("_current", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += "_current";
+
+}
+var slideIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+    showDivs(slideIndex);
+}
